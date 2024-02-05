7d97824aa6a934ec9881cadc5018a3c3aecb9886
shinhwiii <sh0122451@gmail.com>
2024-02-05 22:37:53 +0900

    feat: 타깃 설정

--- Assets/_My/Scripts/PlayerManager.cs
+++ Assets/_My/Scripts/PlayerManager.cs
@@ -11,6 +11,12 @@ public class PlayerManager : MonoBehaviour
     private CinemachineVirtualCamera aimCam;
     [SerializeField]
     private GameObject aimImage;
+    [SerializeField]
+    private GameObject aimObj;
+    [SerializeField]
+    private float aimObjDis = 10f;
+    [SerializeField]
+    private LayerMask targetLayer;
 
     private void Start()
     {
@@ -28,6 +34,27 @@ public class PlayerManager : MonoBehaviour
         {
             aimCam.gameObject.SetActive(true);
             aimImage.SetActive(true);
+
+            Vector3 targetPosition = Vector3.zero;
+            Transform camTransform = Camera.main.transform;
+            RaycastHit hit;
+
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, Mathf.Infinity, targetLayer))
+            {
+                targetPosition = hit.point;
+                aimObj.transform.position = hit.point;
+            }
+            else
+            {
+                targetPosition = camTransform.position + camTransform.forward * aimObjDis;
+                aimObj.transform.position = camTransform.position + camTransform.forward * aimObjDis;
+            }
+
+            Vector3 targetAim = targetPosition;
+            targetAim.y = transform.position.y;
+            Vector3 aimDir = (targetAim - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * 50f);
         }
         else
         {
